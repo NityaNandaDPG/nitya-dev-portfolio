77f8a9216ed1dc925928cb1db4db2662d0cde4c2
NityaNanda Adhikary <nnadkbdhupguri@gmail.com>
2025-03-09 01:40:52 +0530

    sitemap, helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Full-Stack & Android App Developer | React, Java Spring Boot, Firebase</title>
+
+        <meta name="description" content="Freelance Full-Stack & Android App Developer skilled in React, Node.js, Java Spring Boot, MySQL, MongoDB, and Firebase. Specializing in scalable web & mobile applications, API integration, and cloud solutions to deliver high-performance, user-friendly digital experiences." />
+        <meta name="keywords" content="Full-Stack Developer, Android App Developer, React Developer, Java Spring Boot, Node.js, MySQL, MongoDB, Firebase, Web Development, Mobile App Development, API Integration, Cloud Solutions, Freelance Developer" />
+
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
