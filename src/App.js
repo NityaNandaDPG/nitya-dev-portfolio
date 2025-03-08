46c4cb9098daa30de680ecc4d36757ae5b8f3097
NityaNanda Adhikary <nnadkbdhupguri@gmail.com>
2025-03-09 02:08:42 +0530

    added cname for not add custom domain repeatedly after every deployment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <title>Full-Stack & Android App Developer | React, Java Spring Boot, Firebase</title>
 
         <meta name="description" content="Freelance Full-Stack & Android App Developer skilled in React, Node.js, Java Spring Boot, MySQL, MongoDB, and Firebase. Specializing in scalable web & mobile applications, API integration, and cloud solutions to deliver high-performance, user-friendly digital experiences." />
-        <meta name="keywords" content="Full-Stack Developer, Android App Developer, React Developer, Java Spring Boot, Node.js, MySQL, MongoDB, Firebase, Web Development, Mobile App Development, API Integration, Cloud Solutions, Freelance Developer" />
+        <meta name="keywords" 
+          content="nitya-dev, nityadev, nitya dev, nitya dev online, nityadev online, nitya online, dev online, nityadevonline, nitya devonline, dev nitya, nityananda adhikary, freelancer, freelancer west bengal, west bengal freelancer, nitya bangla, nitya bengali, nitya nanda dev, nityananda freelancer, Full-Stack Developer, Android App Developer, React Developer, Java Spring Boot, Node.js, MySQL, MongoDB, Firebase, Web Development, Mobile App Development, API Integration, Cloud Solutions, Freelance Developer" />
 
       </Helmet>
       <header className="App-header">
